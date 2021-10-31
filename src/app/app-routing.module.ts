5956a0ca38699e10ab42972547be73642ae22d42
vegatech <itservicios23@gmail.com>
2021-10-31 18:40:27 -0500

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule, Component } from '@angular/core';
+import {RouterModule,Routes} from '@angular/router'
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
+
+
+
+const routes  : Routes =[
+{ path: '',
+  component: PagesComponent,
+  children :[
+    { path: 'dashboard', component: DashboardComponent},
+    { path: 'progress', component: ProgressComponent},
+    { path: 'graficas1', component: Graficas1Component},
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  ]
+},
+
+{ path: 'login', component: LoginComponent},
+{ path: 'register', component: RegisterComponent},
+
+{ path: '**', component: NopagefoundComponent},
+];
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+      RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
---
+++ src/app/pages/nopagefound/nopagefound.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nopagefound',
+  templateUrl: './nopagefound.component.html',
+  styles: [
+  ]
+})
+export class NopagefoundComponent  {
+
+
+   year = new  Date().getFullYear();
+
+}
